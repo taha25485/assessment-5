1048cf143768e7eadfbe43cb6b6d6ffa95642f36
tyler <tyler@velo.org>
2026-03-01 19:35:11 +0900

    initial commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secretKey: 'sdfklsdfslfnlj3j5bj35bj4b4',
+  localDB: 'mongodb://localhost/realestatedb',
+  publicKey: 'aHR0cHM6Ly9hcGkubnBvaW50LmlvL2VmMjg3NWY3MGU1OWUzMTkxODlk'
+}
